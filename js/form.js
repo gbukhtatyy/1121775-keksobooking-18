00f0fd09c466a600571341b3b7a811c7af8b027b
risekit <gbukhtatyy@gmail.com>
2019-09-28 21:04:03 +0300

    Валидация полей тип жилья и цены

--- js/form.js
+++ js/form.js
@@ -8,11 +8,20 @@
     100: [0]
   };
 
+  var TYPE_PRICE_RELATION = {
+    'bungalo': 0,
+    'flat': 1000,
+    'house': 5000,
+    'palace': 10000
+  };
+
   var fieldCapacityElement = document.querySelector('#capacity');
   var fieldRoomNumberElement = document.querySelector('#room_number');
   var fieldAddressElement = document.querySelector('#address');
   var fieldTimeIn = document.querySelector('#timein');
   var fieldTimeOut = document.querySelector('#timeout');
+  var fieldType = document.querySelector('#type');
+  var fieldPrice = document.querySelector('#price');
 
   var validateCapacityField = function () {
     var roomNumber = fieldRoomNumberElement.value;
@@ -31,13 +40,25 @@
     }
   };
 
+  var validatePriceField = function () {
+    var type = fieldType.value;
+
+    var minPrice = TYPE_PRICE_RELATION[type] ? TYPE_PRICE_RELATION[type] : 0;
+
+    fieldPrice.setAttribute('min', minPrice);
+    fieldPrice.setAttribute('placeholder', minPrice);
+  };
+
   fieldRoomNumberElement.addEventListener('change', validateCapacityField);
   fieldCapacityElement.addEventListener('change', validateCapacityField);
+
+  fieldType.addEventListener('change', validatePriceField);
 
   fieldTimeIn.addEventListener('change', validateTimeFields);
   fieldTimeOut.addEventListener('change', validateTimeFields);
 
   validateCapacityField();
+  validatePriceField();
 
   window.form = {
     changeDisabledFormElements: function (form, isDisabled) {
