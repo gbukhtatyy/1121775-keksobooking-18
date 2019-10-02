c0b72aefaced195e5bd627167936de4ab61b375d
risekit <gbukhtatyy@gmail.com>
2019-10-02 17:11:24 +0300

    Убраны магические числа

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,9 @@
 'use strict';
 
 (function () {
+  var XHR_STATUS_SUCCESS = 200;
+  var XHR_TIMEOUT = 1000;
+
   var LOAD_METHOD = 'GET';
   var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
 
@@ -10,7 +13,7 @@
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === XHR_STATUS_SUCCESS) {
         onLoad(xhr.response);
       } else {
         onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
@@ -25,7 +28,7 @@
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
-    xhr.timeout = 10000; // 10s
+    xhr.timeout = XHR_TIMEOUT;
 
     xhr.open(method, url);
 
