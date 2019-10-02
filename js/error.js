eb276a41857f8a4898734dca6c17f7d83b53fe1b
risekit <gbukhtatyy@gmail.com>
2019-10-02 16:33:41 +0300

    Добавлена возможность закрытия сообщения об ошибке

--- js/error.js
+++ js/error.js
@@ -3,12 +3,36 @@
 (function () {
   var mainContainer = document.querySelector('main');
   var template = document.querySelector('#error').content.querySelector('.error');
+  var errorPopup = false;
+
+  var enterEscErrorPopupHanlder = function (evt) {
+    window.util.isEscEvent(evt, function () {
+      window.error.close();
+    });
+  };
+
+  var clickCloseErrorPopupHandler = function () {
+    window.error.close();
+  };
 
   window.error = {
     show: function (message) {
-      var element = template.cloneNode(true);
-      element.querySelector('.error__message').innerHTML = message;
-      mainContainer.append(element);
+      errorPopup = template.cloneNode(true);
+
+      errorPopup.querySelector('.error__message').innerHTML = message;
+
+      mainContainer.append(errorPopup);
+
+      errorPopup.querySelector('.error__button').addEventListener('click', clickCloseErrorPopupHandler);
+      document.addEventListener('keydown', enterEscErrorPopupHanlder);
+      errorPopup.addEventListener('click', clickCloseErrorPopupHandler);
+    },
+    close: function () {
+      errorPopup.addEventListener('click', clickCloseErrorPopupHandler);
+      document.removeEventListener('keydown', enterEscErrorPopupHanlder);
+      errorPopup.querySelector('.error__button').removeEventListener('click', clickCloseErrorPopupHandler);
+
+      errorPopup.remove();
     }
   };
 })();
